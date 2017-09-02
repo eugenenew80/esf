e83bff3e9845c2a0cb88eed2a5c9c17c8effae32
eugenenew80 <eugenenew80@gmail.com>
2017-09-02 11:54:35 +0600

    Fix bug: constraints for Vendor Entity

--- src/main/java/esf/entity/Vendor.java
+++ src/main/java/esf/entity/Vendor.java
@@ -3,6 +3,8 @@ package esf.entity;
 import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import esf.common.entity.HasId;
 import esf.common.entity.HasName;
@@ -20,9 +22,14 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(of= {"id"})
 public class Vendor implements HasId, HasName {
 	private Long id;
-	private String address;
+	
+	@NotNull
 	private String name;
+	
+	@NotNull @Size(min=12, max = 12)
 	private String rnn;
+
+	private String address;
 	private CustomerType status;
 	private String tin;
 	private String countryCode;
